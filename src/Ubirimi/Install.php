8574492ad83495560152c83edea3cc8816917cdb
domnulnopcea <domnulnopcea@gmail.com>
2015-03-04 15:49:16 +0200

    new lock file

--- src/Ubirimi/Install.php
+++ src/Ubirimi/Install.php
@@ -7,6 +7,25 @@ use Composer\Script\Event;
 class Install {
 
     public static function install(Event $event) {
+        $io = $event->getIO();
 
+        $domain =  $io->ask("Domain: ");
+        $adminFirstName =  $io->ask("Administrator First Name: ");
+        $adminLastName =  $io->ask("Administrator Last Name: ");
+        $adminUsername =  $io->ask("Administrator Username: ");
+        $adminPassword =  $io->ask("Administrator Password: ");
+        $adminEmail =  $io->ask("Administrator Password (again): ");
+
+        $data = json_encode(array(
+            'companyDomain' => $domain,
+            'adminFirstName' => $adminFirstName,
+            'adminLastName' => $adminLastName,
+            'adminUsername' => $adminUsername,
+            'adminPass' => $adminPassword,
+            'adminEmail' => $adminEmail
+        ));
+
+
+        exit;
     }
 }
