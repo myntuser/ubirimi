d5eb2fc31af8909458654cda18eff4ef25b336bd
domnulnopcea <domnulnopcea@gmail.com>
2015-03-04 17:14:01 +0200

    install local client procedure - progress

--- src/Ubirimi/Install.php
+++ src/Ubirimi/Install.php
@@ -3,28 +3,32 @@
 namespace Ubirimi;
 
 use Composer\Script\Event;
+use Ubirimi\Container\UbirimiContainer;
 
 class Install {
 
     public static function install(Event $event) {
         $io = $event->getIO();
 
-        $domain =  $io->ask("Domain: ");
-        $adminFirstName =  $io->ask("Administrator First Name: ");
-        $adminLastName =  $io->ask("Administrator Last Name: ");
-        $adminUsername =  $io->ask("Administrator Username: ");
-        $adminPassword =  $io->ask("Administrator Password: ");
-        $adminEmail =  $io->ask("Administrator Password (again): ");
+        $domain = $io->ask("Domain: ");
+        $adminFirstName = $io->ask("Administrator First Name: ");
+        $adminLastName = $io->ask("Administrator Last Name: ");
+        $adminUsername = $io->ask("Administrator Username: ");
+        $adminPassword = $io->ask("Administrator Password: ");
+        $adminEmail = $io->ask("Administrator Password (again): ");
+        $baseURL = $io->ask("Base URL (Ex: http://ubirimi.company.com): ");
 
-        $data = json_encode(array(
+        $cliendData = array(
             'companyDomain' => $domain,
             'adminFirstName' => $adminFirstName,
             'adminLastName' => $adminLastName,
             'adminUsername' => $adminUsername,
             'adminPass' => $adminPassword,
-            'adminEmail' => $adminEmail
-        ));
+            'adminEmail' => $adminEmail,
+            'baseURL' => $baseURL
+        );
 
+        UbirimiContainer::get()['client']->add($cliendData);
 
         exit;
     }
